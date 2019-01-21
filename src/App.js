ef3d6027eeb5f1e6049e529fd0ebfabe89f3dcc4
cabonkasia <cabonkc@gmail.com>
2019-01-21 11:27:58 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import DogsListContainer from './components/DogsListContainer'
+import { Route } from 'react-router-dom'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer'
 
 class App extends Component {
   render() {
     return (
       <main>
-        <DogsListContainer />
-      </main>
+        <Route exact path="/" component={DogsListContainer} />
+        <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />    
+        </main>
     );
   }
 }
